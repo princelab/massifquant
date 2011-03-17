9b5f2d17aeb5238553bd5be6146845eacbd3786e
John Prince <jtprince@gmail.com>
2011-03-16 23:51:35 -0600

    a little progress

--- lib/kalmanquant/tracker.rb
+++ lib/kalmanquant/tracker.rb
@@ -1,15 +1,34 @@
+require 'matrix'
+
 class Matrix
   # A\B == A^-1 * B
   def backslash(other)
     self.inverse * other
   end
+
+  def []=(i, j, x)
+    @rows[i][j] = x
+  end
 end
 
-
 module Kalmanquant
+  # finds a large peak and sets reasonable estimates for variances
+  GhostScan = Struct.new(:mz_var, :int_var, :max_int) do
+    # returns a GhostScan object based on the spectra
+    def self.new(spectra)
+    end
+
+    def self.default_vals
+      self.new(5.7742e-07, 4.9362e07, 31203.35003809)
+    end
+  end
   class Tracker
 
-    MZQ = Matrix.identity(2) * 10e-4
+    MZQ = Matrix.zero(2)
+    G = Matrix[[0, 0]]
+    U = MZG.transpose
+    G_X_U = G * U
+
     # throws away velocity component (a column vector) (MZH in chris's code)
     H_TRANSFORM = Matrix[[1, 0]]
     H_TRANSFORM_TRANSPOSE = H_TRANSFORM.transpose
@@ -52,6 +71,9 @@ module Kalmanquant
       MZQ
     end
 
+    # the total number of spectra seen (sum of updates and misses)
+    attr_reader :num_spectra_total
+
     # a ghost scan Chris found:
     # m/zs (just 5 values):   [794.4053 794.4046 794.4048 794.4033 794.4050]
     # intensities:  [   1.2684 2.1472 3.1203 2.4196 1.7210 ] * 10e4
@@ -59,12 +81,12 @@ module Kalmanquant
     # MZVar: 5.7742e-07  # could square root this (which would make it larger)
     # MaxIntSqrt: 176.6447
 
-
-    def initialize(peak, delta_time, ghost_mz_var=0.1, ghost_int_var=1000, ghost_max_int_sqrt=1)
+    def initialize(peak, current_time, ghost_scan=GhostScan.default_vals)
+      @times << current_time
       @peaks = [peak] 
-      @mzr = ghost_mz_var
-      @intq = Matrix.identity(2) * ghost_int_var
-      @intr = ghost_max_int_sqrt
+      @mzr = Math.sqrt(ghost_scan.mz_var)
+      @intq = Matrix.identity(2) * ghost_scan.int_var
+      @intr = Math.sqrt(ghost_scan.max_int)
       @mz_pred = Matrix[[peak.mz],[0]]
       @int_pred = peak.intensity
       @spectrum_ids = [spectrum_id]
@@ -78,50 +100,75 @@ module Kalmanquant
       @max_int_sqrt = max_int_sqrt
     end
 
-    # need to call prediction step on ALL active trackers  (NEED TO KEEP TRACK
-    # of the TIMES so that the DT is accurate!!!!!!!!!!!!! (for trackers that
-    # missed)] 
-    def prediction!(delta_time)
-      # precisely the same conceptually between Chris and Ralf
-        # %prediction mass
-        # tr.MZP = tr.MZF*tr.MZP*tr.MZF' + tr.MZQ;
-        # tr.MZXhat = tr.MZF*tr.MZXhat + tr.MZG*tr.MZU;
+    # returns self
+    def prediction!(time)
+      # this is not in Chris's code
+      delta_time = (time - @times.last).abs
+      @mzf[0,1] = delta_time
+      @intf[0,1] = delta_time
+      # prediction mass
+      @mzp = @mzf*@mzp*@mzf.transpose + MZQ
+      # the bit on the end about mzg and mzu is not really doing anything ......
+      # tr.MZXhat = tr.MZF*tr.MZXhat + tr.MZG*tr.MZU;
+      @mz_pred = @mzf*@mz_pred + G_X_U
 
-        # %prediction intensity
-        # tr.IntP = tr.IntF*tr.IntP*tr.IntF' + tr.IntQ;
-        # tr.IntXhat = tr.IntF*tr.IntXhat + tr.IntG*tr.IntU;
+      # prediction intensity
+      #tr.IntP = tr.IntF*tr.IntP*tr.IntF' + tr.IntQ;
+      @intp  = @intf*@intp*@intf.transpose + @intq
+      # the bit on the end is not doing anything
+      # tr.IntXhat = tr.IntF*tr.IntXhat + tr.IntG*tr.IntU;
+      @int_pred = @intf*@int_pred+ G_X_U
+      self
     end
 
     def mz_margin ; Math.sqrt(3*@mzp[0,0]) end
     def int_margin ; Math.sqrt(3*@intp[0,0]) end
 
-    # the low m/z cutoff for predicted peaks
-    def pred_lo_mz ; @mz_pred[0,0] - mz_margin end
-    # the high m/z cutoff for predicted peaks
-    def pred_hi_mz ; @mz_pred[0,0] + mz_margin end
-    def pred_lo_int ; @int_pred[0,0] - int_margin end
-    def pred_hi_int ; @int_pred[0,0] + int_margin end
+    # returns the lo and hi predictions
+    def pred_mz_margins
+      mz_m = mz_margin
+      mz_pred_00 = @mz_pre[0,0]
+      [mz_pred_00-mz_m, mz_pred_00+mz_m]
+    end
 
-    def missed! ; @num_missed += 1 end
+    def pred_int_margins
+      int_m = int_margin
+      int_pred_00 = @int_pre[0,0]
+      [int_pred_00-int_m, int_pred_00+int_m]
+    end
 
-    # Cinf=(m/z(p)-m/z(m))^2/std(m/z)+(I(p)-I(m))^2/std(I);
+    def missed! 
+      @num_spectra_total += 1
+      @num_missed += 1 
+    end
+
     def normalized_distance(mz, int)
+      # Cinf=(m/z(p)-m/z(m))^2/std(m/z)+(I(p)-I(m))^2/std(I);
       ((mz - @mz_pred)**2 / Math.sqrt(@mzp[0,0])) + ((int - @int_pred)**2 / Math.sqrt(@intp[0,0]))
     end
 
+    # this should be deprecated in favor of asking if normalized_distance is
+    # close enough.
     def in_bounds?(mz, int)
+      (pred_lo_mz, pred_hi_mz) = pred_mz_margins
+      (pred_lo_int, pred_hi_int) = pred_int_margins
       (mz >= pred_lo_mz) && (mz <= pred_hi_mz) && (int >= pred_lo_int) && (int <= pred_hi_int)
     end
 
     # updates current position (i.e., the innovation step) based on the new peak
-    # mzp, mz_pred, intp, int_pred
+    # [mzp, mz_pred, intp, int_pred].  Sets num_missed back to zero. returns
+    # self
     def update!(peak, current_time)
+      @num_spectra_total += 1
       # Ralf and Chris do this slightly differently conceptually (although it
       # should be equivalent based on Jeff's paper)
       @peaks << peak
+      @times << current_time
+      @num_missed = 0
       @mzp = (@mzp.inverse + H_TRANSFORM_TRANSPOSE * @mzr.backslash(H_TRANSFORM)).inverse
       @mz_pred = @mz_pred - @mzp*(H_TRANSFORM_TRANSPOSE/@mzr) * (H_TRANSFORM*@mz_pred - peak.mz)
       @int_pred = @int_pred - @intp*(H_TRANSFORM_TRANSPOSE/@intr) * (H_TRANSFORM*@int_pred - peak.intensity)
+      self
     end
 
   end
--- lib/kalmanquant/tracker_manager.rb
+++ lib/kalmanquant/tracker_manager.rb
@@ -15,9 +15,9 @@ module Ms
   end
 end
 
-
 module Kalmanquant
   class TrackerManager
+    KEEP_DEAD_RUNTS = true
     MAX_TRACKER_MISSES = 2
     MIN_TRACKER_SIZE = 4
     Match = Struct.new(:tracker, :peak, :distance)
@@ -32,52 +32,55 @@ module Kalmanquant
       is_big_enough = lambda {|tracker| tracker.size >= min_size }
 
       active_trackers = []
+      new_trackers = []
       tracker_heaven = []
-      #avg_delta_time = @spectra.map(:time).each_cons(2) {|a,b| b-a }.inject(:+) / @spectra.size-1
-      #prev_time = @spectra.first.time - avg_delta_time
+      trackers_died_small = []
+
       @spectra.each do |spectrum|
-        #delta_time = spectrum.time - prev_time
-        (matched_trackers, unmatched_trackers, mz_index_set) = partition!(spectrum, active_trackers)
+        active_trackers.each {|tracker| tracker.predict!(spectrum.time) }
+
+        (matched_trackers, unmatched_trackers, matched_peaks, unmatched_peaks) = compete(spectrum, active_trackers + new_trackers)
+        unmatched_trackers.each {|tracker| tracker.missed! }
+
+        # send dead trackers to trash or tracker heaven
         (still_alive, dead) = unmatched_trackers.partition {|tracker| tracker.num_missed <= max_misses }
-        (big_enough, died_miserly) = dead.partition &is_big_enough
-        tracker_heaven.push(*big_enough)
+        # we divide out the dead in case we don't want to keep little trackers
+        # around out of memory concerns
+        (big_enough, not_big_enough) = dead.partition &is_big_enough
+        tracker_heaven.push(*big_enough) 
+        trackers_died_small.push(*not_big_enough) if KEEP_DEAD_RUNTS
 
         # create a new tracker for each unclaimed mz index
-        new_trackers = []
-        (0...spectrum.size).each do |mz_i|
-          unless mz_index_set.include?(mz_i)
-            new_trackers << Kalmanquant::Tracker.new(peak, 'mz_var_placeholder', 'int_var_placeholder', 'max_int_sqrt_placeholder') )
-          end
+        new_trackers = unmatched_peaks.map do |peak|
+          Kalmanquant::Tracker.new(peak, spectrum.time, ghost_peak)
         end
-        # do new predictions for all active trackers
-        (matched_trackers + still_alive).each {|tracker| tracker.predict! }
-        active_trackers = matched_trackers + still_alive + new_trackers
+
+        matched_trackers.zip(matched_peaks) do |tracker,peak|
+          tracker.update!(peak, spectrum.time)
+        end
+
+        active_trackers = matched_trackers + still_alive
       end
-      (big_enough, too_small) = active_trackers.partition &is_big_enough
+      # 2nd coming ... judgement for everyone! hurray
+      (big_enough, too_small) = (active_trackers+new_trackers).partition &is_big_enough
       tracker_heaven.push(*big_enough)
     end
 
-    # greedy algorithm based on minimizing match distance and then negative
-    # tracker.size.  returns a set of matching trackers, a list of unmatched
-    # trackers, and the set of taken indices. As a side-effect, updates the
-    # trackers info upon a match or not
-    def partition!(spectrum, trackers)
-      # also sort by confidence size (prefer smaller confidence intervals)
+    def compete(spectrum, trackers)
       sorted_matches = possible_matches(spectrum, trackers).sort_by {|match| [match.distance, match.tracker.confidence_size, -match.tracker.size] }
-      taken_trackers = Set.new
-      taken_peaks = Set.new
-      sorted_matches.each do |match|
-        unless taken_trackers.include?(match.tracker) || taken_peaks.include?(match.peak)
-          peak = match.peak
-          tracker = match.tracker
-          tracker.update!(peak, spectrum.time)  # victory!
-          taken_trackers.add(tracker)
-          taken_peaks.add(peak)
+      set_of_matched_trackers = Set.new
+      set_of_matched_peaks = Set.new
+      matched_trackers, matched_peaks, unmatched_trackers, unmatched_peaks = [], [], [], []
+      sorted_matches.each do |match| 
+        tracker, peak = match.tracker, match.peak
+        if set_of_matched_trackers.include?(tracker) || set_of_matched_peaks.include?(peak)
+          unmatched_trackers << tracker && unmatched_peaks << peak
+        else
+          set_of_matched_trackers << tracker && set_of_matched_peaks << peak
+          matched_trackers << tracker && matched_peaks << peak
         end
       end
-      unmatched = trackers - taken_trackers
-      unmatched.each {|tracker| tracker.missed! }
-      [taken_trackers, unmatched, taken_peaks]
+      [matched_trackers, unmatched_trackers, matched_peaks, unmatched_peaks]
     end
 
     # returns an array of matches
@@ -87,14 +90,17 @@ module Kalmanquant
       # brute force with binary search algorithm
       possible_matches = []
       trackers.each do |tracker|
-        lo_mz_i = Ms::Support::BinarySearch.search_lower_boundary(mzs) {|mz| mz <=> tracker.lo_mz }
+        (lo_mz, hi_mz) = tracker.pred_mz_margins
+        lo_mz_i = Ms::Support::BinarySearch.search_lower_boundary(mzs) {|mz| mz <=> lo_mz }
         # we use the range to ensure we only need to search the last part of the array
         if lo_mz_i
-          hi_mz_i = Ms::Support::BinarySearch.search_upper_boundary(mzs, lo_mz_i...mzs.size) {|mz| mz <=> tracker.hi_mz }
+          hi_mz_i = Ms::Support::BinarySearch.search_upper_boundary(mzs, lo_mz_i...mzs.size) {|mz| mz <=> hi_mz }
           hi_mz_i -= 1 if hi_mz_i == mzs.size
         end
         if lo_mz_i && hi_mz_i
           (lo_mz_i..hi_mz_i).each do |i|
+            # the in_bounds? could be asked by checking if the normalized
+            # distance is within desired bounds ....!!!!
             if tracker.in_bounds?(mzs[i], intensities[i])
               possible_matches << Match.new(tracker, Ms::Spectrum::Peak(spectrum, i), tracker.normalized_distance(mzs[i], intensities[i]))
             end
@@ -107,243 +113,3 @@ module Kalmanquant
 
 end
 
-
-
-
-
-
-=begin
-
-classdef TrackerManager7 < handle
-    %% Makes sure that the current and next scan data points are all
-    % accounted for by the trackers. 
-    % 
-    properties (SetAccess = public)
-        CurrentScanNum
-        CurrentScan = [];
-        ActiveTrackers = [];
-        PICTrackers = [];
-        
-    end
-    methods
-        %% Constructor
-        function trMgr = TrackerManager7(InitScanNum)
-            %note that the scan num specified, may not 
-            %be scan one
-            trMgr.CurrentScanNum = InitScanNum;
-        end
-        function delete(h)
-        end
-        function [ActiveTrIndex PredPtsIndex Error] = predictActiveTrackers(trMgr)
-            %Each tracker can only claim one point; therefore,
-            %the max points claimed will be the number of 
-            %active trackers
-            NumActiveTrackers = size(trMgr.ActiveTrackers,1);
-            PredPtsIndex = zeros(NumActiveTrackers, 1);
-            ActiveTrIndex = PredPtsIndex;
-            Error = PredPtsIndex;
-            for i = 1:NumActiveTrackers
-                %predict confidence intervals for Active Trackers
-                predictCentroid(trMgr.ActiveTrackers(i));
-                [DataPointIdx LeastError]= claimCurrentCentroid(trMgr.ActiveTrackers(i),...
-                                                                trMgr.CurrentScan);
-                %add only if claimed by tracker                             
-                if DataPointIdx > 0                                 
-                    PredPtsIndex(i,1) = DataPointIdx;
-                    ActiveTrIndex(i,1) = i;
-                    Error(i,1) = LeastError;
-                end
-            end
-        end
-        function manageStatus(trMgr, ActiveTrIndex, PredPtsIndex)
-          
-            %Make sure missed scans are accounted for
-            manageMissed(trMgr, ActiveTrIndex == 0);
-            %Call innovation step on all trackers that successfully claimed
-            manageTracked(trMgr, PredPtsIndex(PredPtsIndex ~= 0), ActiveTrIndex ~= 0);
-        end
-        function manageMissed(trMgr, MissedIdx)
-            MissedTrackers = trMgr.ActiveTrackers(MissedIdx);
-            for i = 1:size(MissedTrackers,1)
-                incrementMissed(MissedTrackers(i));
-                assignStatus(MissedTrackers(i));
-            end
-        end
-        function manageTracked(trMgr, DataPtsIndex, SelectedIndex)
-                SelectedTrackers = trMgr.ActiveTrackers(SelectedIndex);
-                for i = 1:size(SelectedTrackers,1)
-                  
-                   resetMissed(SelectedTrackers(i));
-                   incrementTrackerLength(SelectedTrackers(i));
-                   innovateCentroid(...
-                            SelectedTrackers(i), ...
-                            trMgr.CurrentScanNum, ...
-                            DataPtsIndex(i), ...
-                            trMgr.CurrentScan( DataPtsIndex(i), :)...
-                            );     
-                    assignStatus(SelectedTrackers(i));     
-                end
-                %Mark all tracked points unavailable to new trackers
-                trMgr.CurrentScan(DataPtsIndex, :) = Inf;
-        end
-        function cleanActiveTrackers(trMgr, TrackerStatus)
-            
-            %initialize some arbitrarily large vectors to 
-            %avoid dynamic array in loop
-            maxScanPoints = 10^5;
-            DeleteTrackerIndex = zeros(maxScanPoints,1);
-            PICTrackersIndex = zeros(maxScanPoints,1);
-            
-            %do this so reshuffling only happens once
-            for i = 1:size(trMgr.ActiveTrackers,1)
-                if trMgr.ActiveTrackers(i).Status == TrackerStatus.NOISE
-                    DeleteTrackerIndex(i) = i;
-                elseif (trMgr.ActiveTrackers(i).Status == TrackerStatus.PIC) 
-                    PICTrackersIndex(i) = i;
-                end
-            end
-           
-           %remember to remove zero elements from preallocation
-           
-           %Store Pure Ion Chromatogram (PIC)   
-           %Consider removing peaks below a certain threshold.
-           trMgr.PICTrackers = [trMgr.PICTrackers; ...
-               trMgr.ActiveTrackers(PICTrackersIndex(PICTrackersIndex ~= 0), 1)];
-           
-           %Remove bad trackers and PIC trackers from list
-           RmIndex = union(DeleteTrackerIndex(DeleteTrackerIndex ~= 0), ...
-                           PICTrackersIndex);
-           trMgr.ActiveTrackers = trMgr.ActiveTrackers(...
-                                 setdiff(1:length(trMgr.ActiveTrackers),...
-                                         RmIndex) ...
-                                                   );
-        end
-        function initializeNewTrackers(trMgr, ghost, dt)
-            %@param: 
-            %       trMgr-> TrackerManager trMgrect
-            %       ghost -> the initializer trMgrect
-            %returns: nothing 
-            
-            %determine # of trackers to add
-            ToClaim = trMgr.CurrentScan(:,2) ~= Inf;
-            DataPoints = trMgr.CurrentScan(ToClaim,:);
-            DataPointLength = size(DataPoints, 1);
-            DataIndex = find(ToClaim);
-            
-            %no new trackers to add
-            if (DataPointLength == 0)
-               return; 
-            end
-            
-            %every Tracker has common initialization
-            Init = [ghost.MZVar, ghost.IntVar, ghost.MaxIntSqrt, trMgr.CurrentScanNum, dt];
-               
-			NewTrackers(DataPointLength,1) = Tracker7();
-			trMgr.ActiveTrackers = [trMgr.ActiveTrackers; NewTrackers];
-			   
-            %every tracker has unique elements of initialization
-            %id, mzseed, intseed, data num
-            NewEnd =  size(trMgr.ActiveTrackers,1);
-            NewStart = NewEnd - DataPointLength + 1;
-            j = 1; %sub index for DataPointIndex
-            for i = NewStart:NewEnd
-                copyCommonInit(trMgr.ActiveTrackers(i, 1), Init);
-                %all the following properties are dependent upon index: j
-                trMgr.ActiveTrackers(i).MZXhat(1) = DataPoints(j, 1);
-                trMgr.ActiveTrackers(i).IntXhat(1) = DataPoints(j, 2);
-                trMgr.ActiveTrackers(i).DataPointNumbers(1) = DataIndex(j);
-                trMgr.ActiveTrackers(i).ID = [trMgr.CurrentScanNum; DataIndex(j)]; 
-                j = j + 1;
-            end
-        end
-        function nonzerosPIC(trMgr)
-           %clean up all the zeros in the trMgr at the end of the entire
-           %scan
-            for i = 1:size(trMgr.PICTrackers, 1)
-                %% Data Structures to be kept in final implementation
-                trMgr.PICTrackers(i).DataPointNumbers = ...
-                    nonzeros(trMgr.PICTrackers(i).DataPointNumbers);
-                trMgr.PICTrackers(i).ScanNumbers = ...
-                    nonzeros(trMgr.PICTrackers(i).ScanNumbers);
-              
-                %% DEBUG MODE, temporary structures for figures
-               trMgr.PICTrackers(i).MZPred = ...
-                nonzeros(trMgr.PICTrackers(i).MZPred);
-               trMgr.PICTrackers(i).IntPred = ...
-                nonzeros(trMgr.PICTrackers(i).IntPred);                
-                
-                trMgr.PICTrackers(i).MZInno = ...
-                    nonzeros(trMgr.PICTrackers(i).MZInno);
-                trMgr.PICTrackers(i).IntInno = ...
-                    nonzeros(trMgr.PICTrackers(i).IntInno);
-                               
-                trMgr.PICTrackers(i).TempPMZ = ...
-                    nonzeros(trMgr.PICTrackers(i).TempPMZ);
-                trMgr.PICTrackers(i).TempPInt = ...
-                    nonzeros(trMgr.PICTrackers(i).TempPInt);
-                
-                trMgr.PICTrackers(i).InnoPMZ = ...
-                    nonzeros(trMgr.PICTrackers(i).InnoPMZ);
-                trMgr.PICTrackers(i).InnoPInt = ...
-                    nonzeros(trMgr.PICTrackers(i).InnoPInt);
-                
-                tmpLeft = ...
-                   nonzeros(trMgr.PICTrackers(i).MZConf(:,1));
-                tmpRight = ...
-                   nonzeros(trMgr.PICTrackers(i).MZConf(:,2));
-                trMgr.PICTrackers(i).MZConf = [tmpLeft tmpRight];
-                
-                tmpUp = ...
-                   nonzeros(trMgr.PICTrackers(i).IntConf(:,1));
-                tmpDown = ...
-                    nonzeros(trMgr.PICTrackers(i).IntConf(:,2));
-                trMgr.PICTrackers(i).IntConf = [tmpUp tmpDown];      
-            end
-        end
-    end
-    methods (Static)
-        %% Note I have never seen this function be used
-        %Resolve Collisions
-        function Map = competeActiveTrackers(Map, Error) 
-             if TrackerManager7.existsCollision(nonzeros(Map)) == 1
-                 Display('Resolving');
-                 
-                OneToOneMap = unique(Map);
-                ItrEnd = size(OneToOneMap,1);
-                if (OneToOneMap(1) == 0)
-                   ItrEnd = Itr -1;
-                end
-                
-                for i = 1:ItrEnd
-                   %Get all indices of each unique point claimed
-                   CollideIndex = find(Map == i);
-                   %Two Trackers Claimed
-                   if size(CollideIndex,1) > 1
-
-                      %determine best tracker by least Error error in claim
-                      [LeastError SubIndex] = min(Error(CollideIndex,1));
-                      RejectSubIndex = find(CollideIndex ~= CollideIndex(SubIndex));
-
-                      %make the datapoint inacessible to unsuccessful trackers
-                      Map(CollideIndex(RejectSubIndex),1) = 0;
-                   end
-                end
-
-                if TrackerManager7.existsCollision(Map) == 1
-                    Display('Error in Resolving Collision: every centroid not resolved');
-                end
-             end
-        end
-        function TF = existsCollision(Map)
-            %Is this a one-to-one mapping or not?
-            %Assume it is not
-            TF = 0;
-            if size(Map, 2) ~= size(unique(Map),2)
-                TF = 1;
-                return;
-            end
-        end
-    end
-end %classdef
-
-=end
